e1b9d638c1abcf2057e97f8559ca72ed85349ef7
ManuelM512 <manuel.mongelos@correo.ucu.edu.uy>
2024-07-30 13:21:16 -0300

    Loading from json

--- prediction_data_loader.py
+++ prediction_data_loader.py
@@ -1,67 +1,9 @@
 import pandas as pd
 
 
-# TODO: La idea sería después cambiar a que esto se cargue directo desde jsons
-def get_data():
-    year = 2024
-    round_number = 14
-    circuit_id = 13
-    driver_ids = [
-        1,
-        830,
-        847,
-        852,
-        815,
-        817,
-        857,
-        858,
-        855,
-        846,
-        848,
-        822,
-        807,
-        825,
-        839,
-        842,
-        844,
-        832,
-        840,
-        4,
-    ]
-    constructor_ids = [
-        131,
-        9,
-        131,
-        215,
-        9,
-        215,
-        1,
-        3,
-        15,
-        1,
-        3,
-        15,
-        210,
-        210,
-        214,
-        214,
-        6,
-        6,
-        117,
-        117,
-    ]
-    numbers = [3, 11, 6, 20, 2, 13, 5, 18, 19, 4, 10, 14, 16, 17, 9, 12, 1, 7, 15, 8]
-
-    data_to_predict = {
-        "year": [year] * len(driver_ids),
-        "round": [round_number] * len(driver_ids),
-        "circuitId": [circuit_id] * len(driver_ids),
-        "driverId": driver_ids,
-        "constructorId": constructor_ids,
-        "grid": numbers,
-    }
-
-    return data_to_predict
+def get_data(race_name):
+    df_to_predict = pd.read_csv(f"./race_results/{race_name}")
+    return df_to_predict
 
 
 def get_complementary_data(data: dict):
@@ -90,17 +32,19 @@ def get_complementary_data(data: dict):
         top3s = driver_races["positionOrder"].le(3).astype(int).sum()
         top_3_per_last_year.append((top3s / race_count) * 100)
 
-    # TODO: Optimize this, its counting 2 times the same constructor id...
     top_3_constructors_per_last_year = []
+    constructor_top_3_per_dict = {}
     for constructor_id in constructor_ids:
-        constructor_races = prior_year_df[
-            prior_year_df["constructorId"] == constructor_id
-        ]
-        race_count = constructor_races.shape[0]
-        top3s = constructor_races["positionOrder"].le(3).astype(int).sum()
-        top3s_per_constructor = 0
-        if race_count > 0:
-            top3s_per_constructor = (top3s / race_count) * 100
+        top3s_per_constructor = constructor_top_3_per_dict.get(constructor_id, -1)
+        if top3s_per_constructor == -1:
+            constructor_races = prior_year_df[
+                prior_year_df["constructorId"] == constructor_id
+            ]
+            race_count = constructor_races.shape[0]
+            top3s = constructor_races["positionOrder"].le(3).astype(int).sum()
+            if race_count > 0:
+                top3s_per_constructor = (top3s / race_count) * 100
+            constructor_top_3_per_dict[constructor_id] = top3s_per_constructor
         top_3_constructors_per_last_year.append(top3s_per_constructor)
 
     data["Top 3 Finish"] = top_3_finishes
@@ -110,30 +54,3 @@ def get_complementary_data(data: dict):
     )
 
     return data
-
-
-# TODO: No se por qué no lo puse directo con el get data jajaja
-def get_actual_position_order():
-    actual_results = [
-        1,
-        4,
-        20,
-        16,
-        7,
-        10,
-        2,
-        17,
-        19,
-        5,
-        12,
-        15,
-        18,
-        14,
-        9,
-        13,
-        3,
-        6,
-        11,
-        8,
-    ]
-    return actual_results
